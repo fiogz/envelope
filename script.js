3a0dd0fe455df1600aac54c6fc9c9d19f58cfcfa
fiogz <fiolujan28@gmail.com>
2024-11-06 11:09:15 -0300

    Update script.js

--- script.js
+++ script.js
@@ -19,17 +19,23 @@ envelopeWrapper.addEventListener('click', () => {
     }
 });
 
-// Script para corazones que siguen el cursor
-document.addEventListener('mousemove', function (e) {
+// Evento para corazones que siguen el cursor o toque
+const createHeart = (e) => {
     const body = document.body;
     const heart = document.createElement('span');
     heart.classList.add('cursor-heart');
-    heart.style.left = `${e.pageX}px`;
-    heart.style.top = `${e.pageY}px`;
+    heart.style.left = `${e.pageX || e.touches[0].pageX}px`; // Usar pageX para el mouse o touches[0].pageX para el toque
+    heart.style.top = `${e.pageY || e.touches[0].pageY}px`; // Similar para Y
     const size = Math.random() * 5 + 2;
     heart.style.width = `${size}px`;
     heart.style.height = `${size}px`;
     heart.style.transform = `rotate(${Math.random() * 180}deg)`;
     body.appendChild(heart);
     setTimeout(() => heart.remove(), 1000);
-});
+};
+
+// Detectar movimiento del mouse
+document.addEventListener('mousemove', createHeart);
+
+// Detectar movimiento en pantalla táctil (móviles)
+document.addEventListener('touchmove', createHeart);
